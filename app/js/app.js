04c27472b15f134f6c4c32c028172851b13da527
Scott Mascio <scottmascio@gmail.com>
2015-02-07 17:23:14 -0500

    first commit

--- app/js/app.js
+++ app/js/app.js
@@ -1,23 +1,33 @@
-var app = angular.module("myApp", ["ngRoute", "angularMoment"]);
+var app = angular.module("myApp", ["ngRoute", "angularMoment", 'ui.bootstrap', 'angularTypewrite']);
 
 app.config(function($routeProvider){
   $routeProvider.when('/',
                       {
                         templateUrl: 'partials/app.html',
-                        controller: 'MovieController'
+                        controller: 'CardController'
                       })
 });
 
 app.factory('API', ['$http', function($http) {
   var API = {};
 
-  API.nowPlaying = function(){
+  API.Allcards = function(){
     return $http.get('http://localhost:3000/');
   };
 
   return API;
 }]);
 
-app.controller('MovieController', ['$scope', 'API', function($scope, MOVIES){
-
+app.controller('CardController', ['$scope', 'API', function($scope, CARDS){
+  $scope.myInterval = 2500;
+  var slides = $scope.slides = [];
+  $scope.addSlide = function() {
+    var newWidth = 600 + slides.length + 1;
+    slides.push({
+      image: 'http://placekitten.com/' + newWidth + '/300',
+    });
+  };
+  for (var i=0; i<4; i++) {
+    $scope.addSlide();
+  }
 }]);
